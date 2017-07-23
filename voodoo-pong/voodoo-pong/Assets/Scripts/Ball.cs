d1de4e40f1fee65014cf2649d79984ae812acfd2
Estelle Martinez <rikku.shimizu@gmail.com>
2017-07-23 18:21:19 +0200

    Basic pong gameplay

---
+++ voodoo-pong/voodoo-pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private const float speed = 4f;
+    private const float velocityMultiplier = 0.05f;
+
+    [SerializeField]
+    private Rigidbody2D rigibody;
+
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    private bool isMoving = false;
+    private Vector2 currentVelocity;
+
+    private void SetVelocity(float x, float y)
+    {
+        currentVelocity = new Vector2(x, y);
+        rigibody.velocity = currentVelocity;
+    }
+
+    public void StartGame()
+    {
+        SetVelocity(speed, 0);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            var ballPosition = rectTransform.localPosition.y;
+            var paddlePosition = (collision.transform as RectTransform).localPosition.y;
+            var diff = ballPosition - paddlePosition;
+            var velocityY = velocityMultiplier * diff;
+            var velocityX = Mathf.Abs(speed - velocityY);
+
+            if (currentVelocity.x >= 0) {
+                velocityX *= -1;
+            }
+
+            SetVelocity(velocityX, velocityY);
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            SetVelocity(currentVelocity.x, currentVelocity.y * -1);
+        }
+    }
+}
---
+++ voodoo-pong/voodoo-pong/Assets/Scripts/PlayerArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerArea : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform area;
+
+    [SerializeField]
+    private RectTransform paddle;
+
+    public void MovePaddle(BaseEventData baseEventData)
+    {
+        var eventData = baseEventData as PointerEventData;
+        var newY = eventData.position.y / Screen.height;
+        paddle.anchorMin = new Vector2(paddle.anchorMin.x, newY);
+        paddle.anchorMax = new Vector2(paddle.anchorMax.x, newY);
+    }
+}
